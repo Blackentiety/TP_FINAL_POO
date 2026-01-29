a240f7f7f7c3afbbbf99c62ef43345483e8f5f84
Lukas <lukas.boudinl@gmail.com>
2026-01-29 15:27:31 +0100

    connexion BDD

--- CubicMarket/config/db.php
+++ CubicMarket/config/db.php
@@ -1,1 +1,16 @@
 <?php
+
+$host ="mysql:host=localhost;dbname=cubicMarket;charset=utf8";
+$user ="root";
+$password ="";
+
+try {
+    $db = new PDO($host,$user,$password);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//    echo "Connected successfully";
+
+} catch (PDOException $e) {
+    die ("Connection failed ");
+}
