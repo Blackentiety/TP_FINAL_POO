6ff0fd90a2637aa01d2d5c99dc8b402a7ba86b17
yelnnee <y.adouane@ecole-ipssi.net>
2026-01-29 16:06:08 +0100

    Ajout des Getters et Setters pour la class Grade

--- CubicMarket/src/Entities/shop/Grade.php
+++ CubicMarket/src/Entities/shop/Grade.php
@@ -1,6 +1,26 @@
 <?php
+namespace src\Entities\shop;
+class Grade extends Produit {
+    private $idGrade;
+    private $nomGrade;
+    private $privilege;
 
-class Grade
-{
-
+    public function getIdGrade() {
+        return $this->idGrade;
+    }
+    public function getNomGrade() {
+        return $this->nomGrade;
+    }
+    public function getPrivilege() {
+        return $this->privilege;
+    }
+    public function setIdGrade($idGrade) {
+        $this->idGrade = $idGrade;
+    }
+    public function setNomGrade($nomGrade) {
+        $this->nomGrade = $nomGrade;
+    }
+    public function setPrivilege($privilege) {
+        $this->privilege = $privilege;
+    }
 }
